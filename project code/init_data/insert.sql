e1b7bb265e9c0a1a9c50b9e4cc5cc6ba0fcc8411
James Gashi <james.gashi@colorado.edu>
2023-04-17 14:11:49 -0600

    created database skeleton

--- project code/init_data/insert.sql
+++ project code/init_data/insert.sql
@@ -1,1 +1,4 @@
 -- Create data for the database
+INSERT INTO students (StudentID, first_name, last_name, pwd, email) VALUES (1101, "Guy", "Fawkes", "london", "guyfawkes@colorado.edu");
+INSERT INTO csel (TableID, bookingTime) VALUES (0, "11:00");
+INSERT INTO student_tables (TableID, StudentID) VALUES (0, 1101);

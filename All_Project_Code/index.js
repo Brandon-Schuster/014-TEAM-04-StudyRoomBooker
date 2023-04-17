915d878890a80adc7e7deb44bc957b55f795a187
Brandon-Schuster <brsc5231@colorado.edu>
2023-04-17 16:33:49 -0600

    Pages are now displayed

--- All_Project_Code/index.js
+++ All_Project_Code/index.js
@@ -61,53 +61,98 @@ app.get("/login", (req, res) => {
 });
 
 // Login submission
-// app.post("/login", (req, res) => {
-//   const email = req.body.email;
-//   const username = req.body.username;
-//   const query = "select * from students where students.email = $1";
-//   const values = [email];
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const username = req.body.username;
+  const query = "select * from students where students.email = $1";
+  const values = [email];
 
   // get the student_id based on the emailid
-//   db.one(query, values)
-//     .then((data) => {
-//       user.student_id = data.student_id;
-//       user.username = username;
-//       user.first_name = data.first_name;
-//       user.last_name = data.last_name;
-//       user.email = data.email;
-//       user.year = data.year;
-//       user.major = data.major;
-//       user.degree = data.degree;
+  db.one(query, values)
+    .then((data) => {
 
-//       req.session.user = user;
-//       req.session.save();
+      // students.StudentId = data.StudentId;
+      // students.first_name = data.first_name;
+      // students.last_name = data.last_name;
+      // students.email = data.email;
+      // students.pwd = data.pwd;
 
-//       res.redirect("/");
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.redirect("/login");
-//     });
-// });
+      // req.session.students = students;
+      req.session.save();
 
-// // Authentication middleware.
-// const auth = (req, res, next) => {
-//   if (!req.session.user) {
-//     return res.redirect("/login");
-//   }
-//   next();
-// };
-
-// app.use(auth);
-
-app.get('/welcome', (req, res) => {
-  res.json({status: 'success', message: 'Welcome!'});
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/login");
+    });
 });
+
+// Authentication middleware.
+const auth = (req, res, next) => {
+  if (!req.session.student) {
+    return res.redirect("/login");
+  }
+  next();
+};
+app.use(auth);
+
+app.get("/", (req, res) => {
+  res.render("pages/home", {
+    StudentID: req.session.user.StudentID,
+    first_name: req.session.user.first_name,
+    last_name: req.session.user.last_name,
+    email: req.session.user.email,
+    pwd: req.session.user.pwd,
+  });
+});
+
+app.get("/home", (req, res) => {
+  const taken = req.query.taken;
+  // Query to list all the courses taken by a student
+
+  // db.any(taken ? student_courses : all_courses, [req.session.user.student_id])
+  //   .then((courses) => {
+  //     res.render("pages/courses", {
+  //       courses,
+  //       action: req.query.taken ? "delete" : "add",
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     res.render("pages/courses", {
+  //       courses: [],
+  //       error: true,
+  //       message: err.message,
+  //     })
+    // });
+});
+
 
 
 app.get("/logout", (req, res) => {
   req.session.destroy();
   res.render("pages/logout");
+});
+
+
+
+
+
+
+
+
+
+app.get("/register", (req, res) => {
+  res.render('pages/register');
+});
+
+
+
+
+
+// LEAVE THIS SHIT...please UwU
+app.get('/welcome', (req, res) => {
+  res.json({status: 'success', message: 'Welcome!'});
 });
 
 module.exports = app.listen(3000);

f74c806328cb20adc63d1379dcf8c117cf988669
Noah Garrett <noga2860@colorado.edu>
2023-04-18 16:13:42 -0600

    fixed missing ;  in the create.sql file

--- All_Project_Code/init_data/create.sql
+++ All_Project_Code/init_data/create.sql
@@ -1,5 +1,5 @@
 -- Create Database Tables here
-DROP TABLE IF EXISTS students CASCADE
+DROP TABLE IF EXISTS students CASCADE;
 CREATE TABLE students(
     StudentID INT PRIMARY KEY NOT NULL,
     first_name VARCHAR(60) NOT NULL,
@@ -8,13 +8,13 @@ CREATE TABLE students(
     email VARCHAR(60) NOT NULL
 );
 
-DROP TABLE IF EXISTS csel CASCADE
+DROP TABLE IF EXISTS csel CASCADE;
 CREATE TABLE csel(
     TableID INT PRIMARY KEY NOT NULL,
     bookingTime VARCHAR(60) NOT NULL
 );
 
-DROP TABLE IF EXISTS student_tables CASCADE
+DROP TABLE IF EXISTS student_tables CASCADE;
 CREATE TABLE student_tables(
     TableID INT NOT NULL REFERENCES csel(TableID),
     StudentID INT NOT NULL REFERENCES students(StudentID)

7d7d10d5f503f5a5f9dc9863228cfd9d403dea5b
danielbonnecaze <danielbonnecaze@gmail.com>
2023-04-19 14:54:35 -0600

    Finalize registration skeleton

--- All_Project_Code/src/resources/js/script.js
+++ All_Project_Code/src/resources/js/script.js
@@ -1,1 +1,9 @@
 // JS plugins for HTML page go here
+function pwdMatch(){
+    var pwd = document.getElementById("password");
+    var c_pwd = document.getElementById("confirm_password");
+
+    if(c_pwd != pwd){
+        alert("Password must match")
+    }
+}

95919d387e2e391807f9724459e97722f3f00808
Noah Garrett <noga2860@colorado.edu>
2023-04-20 10:55:51 -0600

    added rooms inserts

--- All_Project_Code/init_data/insert.sql
+++ All_Project_Code/init_data/insert.sql
@@ -3,3 +3,6 @@
 INSERT INTO students (StudentID, first_name, last_name, pwd, email) VALUES (1101, 'Guy', 'Fawkes', '$2a$10$wI3VW/Tm06AYvc92Lx8.revaD/Ge0FN1CLdGAgEym91IcvKM39PSC', 'guyfawkes@colorado.edu');
 INSERT INTO csel (TableID, bookingTime) VALUES (0, '11:00');
 INSERT INTO student_tables (TableID, StudentID) VALUES (0, 1101);
+INSERT INTO rooms (RoomId, RoomCapacity, RoomName) VALUES (1, 5, 'eccs114j');
+INSERT INTO rooms (RoomId, RoomCapacity, RoomName) VALUES (2, 4, 'eccs113j');
+INSERT INTO rooms (RoomId, RoomCapacity, RoomName) VALUES (3, 3, 'eccs112j');

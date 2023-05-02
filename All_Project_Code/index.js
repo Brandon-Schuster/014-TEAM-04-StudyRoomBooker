9bee52912645cdd17d2c45453c99806e1768953d
noahggarrett-1 <noga2860@colorado.edu>
2023-05-01 21:06:20 -0600

    gave each table a unique id which is need to do stuff like disable the tables

--- All_Project_Code/index.js
+++ All_Project_Code/index.js
@@ -96,7 +96,9 @@ app.post('/login', async (req, res) => {
         user.studentid = data.studentid;
         req.session.user = user;
         req.session.save();
-        res.redirect('/home')
+        res.render("pages/home",{
+    formid: req.body.formid,
+  })
       } else {
 
         res.render("pages/login", {
@@ -202,6 +204,7 @@ app.get("/", (req, res) => {
     first_name: req.session.user.first_name,
     last_name: req.session.user.last_name,
     email: req.session.user.email,
+    formid: req.body.formid,
   
   });
 });
@@ -219,7 +222,9 @@ app.get("/profile", (req, res) => {
 
 app.get("/home", (req, res) => {
   const taken = req.query.taken;
-  res.render("pages/home")
+  res.render("pages/home",{
+    formid: res.body.formid,
+  })
   // Query to list all the courses taken by a student
 
   // db.any(taken ? student_courses : all_courses, [req.session.user.student_id])
@@ -348,7 +353,9 @@ app.get("/tableBook", (req, res) => {
 
 app.post("/tableBook", (req, res) => {
 
-  res.redirect("/home");
+  res.render("pages/home",{
+    formid: req.body.formid,
+  })
   const authClient = new google.auth.JWT(
     credentials.client_email,
     null,

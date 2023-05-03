d7ce3e060cdcf19d2c66644fe52e344d6f9456c8
Brandon-Schuster <brsc5231@colorado.edu>
2023-05-02 20:11:04 -0600

    Update to Insert.sql

--- All_Project_Code/init_data/insert.sql
+++ All_Project_Code/init_data/insert.sql
@@ -4,10 +4,10 @@ INSERT INTO students (StudentID, first_name, last_name, pwd, email) VALUES (1101
 INSERT INTO rooms (RoomId, RoomCapacity, RoomName) VALUES (1, 4, 'ECCS114B');
 INSERT INTO rooms (RoomId, RoomCapacity, RoomName) VALUES (2, 4, 'ECCS114C');
 INSERT INTO rooms (RoomId, RoomCapacity, RoomName) VALUES (3, 4, 'ECCS114D');
-INSERT INTO rooms (RoomId, RoomCapacity, RoomName) VALUES (4, 4, 'ECCS114E');
+INSERT INTO rooms (RoomId, RoomCapacity, RoomName) VALUES (4, 16, 'ECCS114E');
 INSERT INTO rooms (RoomId, RoomCapacity, RoomName) VALUES (5, 4, 'ECCS114F');
-INSERT INTO rooms (RoomId, RoomCapacity, RoomName) VALUES (5, 4, 'ECCS114G');
-INSERT INTO rooms (RoomId, RoomCapacity, RoomName) VALUES (5, 4, 'ECCS114H');
-INSERT INTO rooms (RoomId, RoomCapacity, RoomName) VALUES (6, 4, 'ECCS114J');
+INSERT INTO rooms (RoomId, RoomCapacity, RoomName) VALUES (6, 4, 'ECCS114G');
+INSERT INTO rooms (RoomId, RoomCapacity, RoomName) VALUES (7, 4, 'ECCS114H');
+INSERT INTO rooms (RoomId, RoomCapacity, RoomName) VALUES (8, 4, 'ECCS114J');
 INSERT INTO bookings (RoomId, StudentID, BookingStatus, BookingTime) VALUES (1,1101,false,'time');
 --INSERT INTO bookings (RoomId, StudentID, BookingStatus, BookingTime) VALUES (2,1101,false,'time');
